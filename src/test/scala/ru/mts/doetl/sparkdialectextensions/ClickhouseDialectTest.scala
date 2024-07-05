f3e9d780585eaf77d5e0dbab6bf509a76deaa709
maxim-lixakov <mvliksako1@mts.ru>
2024-07-05 17:05:17 +0300

    [DOP-16954] - update ClickhouseFixture

--- src/test/scala/ru/mts/doetl/sparkdialectextensions/ClickhouseDialectTest.scala
+++ src/test/scala/ru/mts/doetl/sparkdialectextensions/ClickhouseDialectTest.scala
@@ -3,7 +3,8 @@ package ru.mts.doetl.sparkdialectextensions
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.apache.spark.sql.jdbc.JdbcDialects
-import org.apache.spark.sql.types.{BooleanType, IntegerType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.types.{BooleanType, ByteType, IntegerType, ShortType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.functions._
 
 import ru.mts.doetl.sparkdialectextensions.clickhouse.{ClickhouseDataframeGenerator, ClickhouseFixture}
 
@@ -66,5 +67,55 @@ class ClickhouseDialectTest extends AnyFunSuite with Matchers with SharedSparkSe
       .option("password", jdbcPassword)
       .mode("append")
       .save()
+
+    val loadedDf = spark.read
+      .format("jdbc")
+      .option("url", jdbcUrl)
+      .option("dbtable", tableName)
+      .load()
+
+    // TODO: remove after implementing custom mapping in ClickhouseDialectExtension
+    val castedLoadedDf = loadedDf.select(
+      col("booleanColumn").cast(BooleanType),
+      col("byteColumn").cast(ByteType),
+      col("shortColumn").cast(ShortType),
+      col("timestampColumn").cast(TimestampType),
+      col("jsonColumn").cast(StringType)
+    )
+
+    assert(castedLoadedDf.schema == df.schema)
+  }
+
+  test("test spark dataframe create and write to new clickhouse table") {
+
+    val generator = new ClickhouseDataframeGenerator(spark)
+    val df = generator.createDataFrame()
+
+    df.write
+      .format("jdbc")
+      .option("url", jdbcUrl)
+      .option("dbtable", tableName)
+      .option("user", jdbcUser)
+      .option("password", jdbcPassword)
+      .option("createTableOptions", "ENGINE = TinyLog")
+      .mode("errorIfExists")
+      .save()
+
+    val loadedDf = spark.read
+      .format("jdbc")
+      .option("url", jdbcUrl)
+      .option("dbtable", tableName)
+      .load()
+
+    // TODO: remove after implementing custom mapping in ClickhouseDialectExtension
+    val castedLoadedDf = loadedDf.select(
+      col("booleanColumn").cast(BooleanType),
+      col("byteColumn").cast(ByteType),
+      col("shortColumn").cast(ShortType),
+      col("timestampColumn").cast(TimestampType),
+      col("jsonColumn").cast(StringType)
+    )
+
+    assert(castedLoadedDf.schema == df.schema)
   }
 }
