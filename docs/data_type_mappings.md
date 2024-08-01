f9e89402ff94c3266001403e80cc40df958b109d
maxim-lixakov <mvliksako1@mts.ru>
2024-08-01 14:35:41 +0300

    [DOP-18631] - add logic for writing ArrayType(...) to Clickhouse Array(...)

--- docs/data_type_mappings.md
+++ docs/data_type_mappings.md
@@ -11,6 +11,20 @@ This documentation outlines the customized mappings that the Spark Dialect Exten
 | `Datetime64(6)`            | `TimestampType`                | `Datetime64(6)`               | `Datetime64(6)`             |
 | `Bool`                     | `BooleanType`                  | `Bool`                        | `Bool`                      |
 
+``Array(T)`` `->` ``ArrayType(T)``:
+
+| ClickHouse Type (Read) | Spark Type                     | ClickHouse Type (Write) | ClickHouse Type (Create) |
+|------------------------|--------------------------------|-------------------------|--------------------------|
+| `Array(String)`        | `ArrayType(StringType)`        | `Array(String)`         | `Array(String)`          |
+| unsupported            | `ArrayType(ByteType)`          | `Array(Int8)`           | `Array(Int8)`            |
+| unsupported            | `ArrayType(ShortType)`         | unsupported             | unsupported              |
+| unsupported            | `ArrayType(LongType)`          | `Array(Int64)`          | `Array(Int64)`           |
+| `Array(Decimal(M, N))` | `ArrayType(DecimalType(M, N))` | `Array(Decimal(M, N))`  | `Array(Decimal(M, N))`   |
+| unsupported            | `ArrayType(TimestampType)`     | unsupported             | unsupported              |
+| unsupported            | `ArrayType(Date)`              | `Array(Date)`           | `Array(Date)`            |
+| unsupported            | `ArrayType(FloatType)`         | `Array(Float32)`        | `Array(Float32)`         |
+| unsupported            | `ArrayType(DoubleType)`        | unsupported             | unsupported              |
+
 
 #### Default Type Mappings without Spark Dialect Extension
 
@@ -20,3 +34,7 @@ This documentation outlines the customized mappings that the Spark Dialect Exten
 | `Int16`                    | `IntegerType`                  | `Int32`                       | `Int32`                     |
 | `Datetime64(6)`            | `TimestampType`                | `Datetime64(6)`               | `DateTime32`                |
 | `Bool`                     | `BooleanType`                  | `Bool`                        | `UInt64`                    |
+
+``Array(T)`` `->` ``ArrayType(T)``:
+
+**unsupported**
--- src/test/scala/ru/mts/doetl/sparkdialectextensions/ClickhouseDialectTest.scala
+++ src/test/scala/ru/mts/doetl/sparkdialectextensions/ClickhouseDialectTest.scala
@@ -227,49 +227,18 @@ class ClickhouseDialectTest
     statement.close()
   }
 
-  val testCases: TableFor3[String, String, DataType] = Table(
+  val testReadArrayCases: TableFor3[String, String, DataType] = Table(
     ("columnDefinition", "insertedData", "expectedType"),
     (
       "charArrayColumn Array(String)",
       "(['a', 'b', 'c', 'd', 'e'])",
       ArrayType(StringType, containsNull = false)),
     (
-      "intArrayColumn Array(Int8)",
-      "([1, 2, 3, 4, 5])",
-      ArrayType(ByteType, containsNull = false)),
-    (
-      "shortArrayColumn Array(Int16)",
-      "([1, 2, 3, 4, 5])",
-      ArrayType(ShortType, containsNull = false)),
-    (
-      "intArrayColumn Array(Int32)",
-      "([1, 2, 3, 4, 5])",
-      ArrayType(IntegerType, containsNull = false)),
-    (
-      "longArrayColumn Array(Int64)",
-      "([1, 2, 3, 4, 5])",
-      ArrayType(LongType, containsNull = false)),
-    (
-      "floatArrayColumn Array(Float32)",
-      "([1.0, 2.0, 3.0, 4.0, 5.0])",
-      ArrayType(FloatType, containsNull = false)),
-    (
-      "doubleArrayColumn Array(Float64)",
-      "([1.0, 2.0, 3.0, 4.0, 5.0])",
-      ArrayType(DoubleType, containsNull = false)),
-    (
-      "dateArrayColumn Array(Date)",
-      "(['2022-01-01', '2022-01-02', '2022-01-03'])",
-      ArrayType(DateType, containsNull = false)),
-    (
-      "timestampArrayColumn Array(DateTime)",
-      "(['2022-01-01 00:00:00', '2022-01-02 00:00:00', '2022-01-03 00:00:00'])",
-      ArrayType(TimestampType, containsNull = false)),
-    (
       "decimalArrayColumn Array(Decimal(9,2))",
       "([1.23, 2.34, 3.45, 4.56, 5.67])",
       ArrayType(DecimalType(9, 2), containsNull = false)))
-  forAll(testCases) { (columnDefinition: String, insertedData: String, expectedType: DataType) =>
+
+  forAll(testReadArrayCases) { (columnDefinition: String, insertedData: String, expectedType: DataType) =>
     test(s"read ClickHouse Array for ${columnDefinition} column") {
       setupTable(columnDefinition)
       insertTestData(Seq(insertedData))
@@ -283,23 +252,56 @@ class ClickhouseDialectTest
       assert(df.schema.fields.head.dataType === expectedType)
 
       df.collect().map(_.getAs[Seq[Any]](df.schema.fields.head.name)).head
-      df.count() == 1
+      assert(df.count() == 1)
     }
   }
 
-  forAll(testCases) { (columnDefinition: String, insertedData: String, expectedType: DataType) =>
-    test(s"write ClickHouse Array for ${columnDefinition} column") {
-      setupTable(columnDefinition)
-      insertTestData(Seq(insertedData))
+  val testWriteArrayCases: TableFor3[String, Seq[Row], DataType] = Table(
+    ("columnDefinition", "insertedData", "expectedType"),
+    (
+      "charArrayColumn Array(String)",
+      Seq(Row(Array("a", "b", "c", "d", "e"))),
+      ArrayType(StringType, containsNull = false)
+    ),
+    (
+      "byteArrayColumn Array(Int8)",
+      Seq(Row(Array(1.toByte, 2.toByte, 3.toByte, 4.toByte, 5.toByte))),
+      ArrayType(ByteType, containsNull = false)
+    ),
+    (
+      "intArrayColumn Array(Int32)",
+      Seq(Row(Array(1, 2, 3, 4, 5))),
+      ArrayType(IntegerType, containsNull = false)
+    ),
+    (
+      "longArrayColumn Array(Int64)",
+      Seq(Row(Array(1L, 2L, 3L, 4L, 5L))),
+      ArrayType(LongType, containsNull = false)
+    ),
+    (
+      "floatArrayColumn Array(Float32)",
+      Seq(Row(Array(1.0f, 2.0f, 3.0f, 4.0f, 5.0f))),
+      ArrayType(FloatType, containsNull = false)
+    ),
+    (
+      "dateArrayColumn Array(Date)",
+      Seq(Row(Array(java.sql.Date.valueOf("2022-01-01"), java.sql.Date.valueOf("2022-01-02"), java.sql.Date.valueOf("2022-01-03")))),
+      ArrayType(DateType, containsNull = false)
+    ),
+    (
+      "decimalArrayColumn Array(Decimal(9,2))",
+      Seq(Row(Array(new java.math.BigDecimal("1.23"), new java.math.BigDecimal("2.34"), new java.math.BigDecimal("3.45"), new java.math.BigDecimal("4.56"), new java.math.BigDecimal("5.67")))),
+      ArrayType(DecimalType(9, 2), containsNull = false)
+    )
+  )
 
-      val df = spark.read
-        .format("jdbc")
-        .option("url", jdbcUrl)
-        .option("dbtable", tableName)
-        .option("wrapperObject", "true")
-        .load()
+  forAll(testWriteArrayCases) { (columnDefinition: String, insertedData: Seq[Row], expectedType: DataType) =>
+    test(s"write ClickHouse Array for $columnDefinition column") {
 
-      assert(df.schema.fields.head.dataType === expectedType)
+
+      val columnName = columnDefinition.split(" ")(0)
+      val schema = StructType(Array(StructField(columnName, expectedType, nullable = false)))
+      val df = spark.createDataFrame(spark.sparkContext.parallelize(insertedData), schema)
 
       df.write
         .format("jdbc")
@@ -307,10 +309,11 @@ class ClickhouseDialectTest
         .option("dbtable", tableName)
         .option("user", jdbcUser)
         .option("password", jdbcPassword)
-        .mode("append")
+        .option("createTableOptions", "ENGINE = TinyLog")
+        .mode("errorIfExists")
         .save()
 
-      df.count() == 2
+      assert(df.count() == 1)
     }
   }
 }
