dfbe00012ea10d748ea932e04af1742ba8b52cd2
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-09-25 10:56:49 +0300

    [DOP-18232] Replace SBT with Gradle

--- src/test/scala/io/github/mtsongithub/doetl/sparkdialectextensions/SharedSparkSession.scala
+++ src/test/scala/io/github/mtsongithub/doetl/sparkdialectextensions/SharedSparkSession.scala
@@ -18,7 +18,12 @@ trait SharedSparkSession extends BeforeAndAfterAll { self: Suite =>
     val scalaVersion: String = scala.util.Properties.versionNumberString
     val majorScalaVersion: String = scalaVersion.split('.').take(2).mkString(".")
     val jarFiles =
-      Paths.get("target", s"scala-$majorScalaVersion").toFile.listFiles().filter(_.isFile)
+      Paths
+        .get("build", "libs")
+        .toFile
+        .listFiles()
+        .filter(_.getName.contains(s"_$majorScalaVersion-"))
+        .filter(_.isFile)
     val jarPaths = jarFiles.map(_.getAbsolutePath).mkString(",")
 
     _spark = SparkSession
@@ -27,7 +32,6 @@ trait SharedSparkSession extends BeforeAndAfterAll { self: Suite =>
       .appName("Spark Test Session")
       .config("spark.ui.enabled", "false") // disable UI to reduce overhead
       .config("spark.jars", jarPaths) // include the JAR file containing the custom dialect
-      .config("spark.driver.bindAddress", "127.0.0.1")
       .getOrCreate()
 
     // register custom Clickhouse dialect
